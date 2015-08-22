ba51d00f5c129f2fb750c17145e4341027599291
Edll <edlly@edlly.de>
2015-08-22 09:39:41 +0200

    Variabel Namen und Konstruktor geändert
    Variabel Namen kleingeschrieben.
    Aus dem Konstruktor die sqlConnection entfernt da die abfrage immer null
    ergeben würde.

--- src/de/edlly/gui/materialVerwaltung/Tab.java
+++ src/de/edlly/gui/materialVerwaltung/Tab.java
@@ -17,180 +17,169 @@ import javax.swing.ScrollPaneConstants;
 import de.edlly.material.DbAbfrage;
 import de.edlly.material.DbHinzu;
 
-
-
 /**
- * Enthält das UI für den Materialmanager. Sowie die ActionListener und die SQL Connection 
+ * Enthält das UI für den Materialmanager. Sowie die ActionListener und die SQL
+ * Connection
  * 
- * Attribute:
- * 	Connection SqlConn SQL Connection
- *  JTextField txtMatAnlex Textfeld für die Material Breite
- *  JTextField txtMatAnlez Textfeld für die Material Tiefe
- *  JTextField txtMatAnleMax Textfeld für die MaxLänge
+ * Attribute: Connection SqlConn SQL Connection JTextField txtMatAnlex Textfeld
+ * für die Material Breite JTextField txtMatAnlez Textfeld für die Material
+ * Tiefe JTextField txtMatAnleMax Textfeld für die MaxLänge
+ * 
  * @author Edlly
  *
  */
 
 public class Tab {
-	
-	public Connection SqlConn=null;
-	
+
+	private Connection sqlConn = de.edlly.db.SQLiteConnect.dbConnection();
+
 	private JTextField txtMatAnlex;
 	private JTextField txtMatAnlez;
 	private JTextField txtMatAnleMax;
-	
+
 	// Konstruktor
-	public Tab(){
-		// SQL Connection herstellen
-		if(this.SqlConn == null){
-			SqlConn = de.edlly.db.SQLiteConnect.dbConnection();
-		}
-		
-		
+	public Tab() {
+
 	}
-	
-	public JPanel ui(){
-		
-		JPanel TabMaterialManager = new JPanel();
-		
+
+	public JPanel ui() {
+
+		JPanel tabMaterialManager = new JPanel();
+
 		// Objekt für die Material Tabelle erzeugen
-		de.edlly.gui.materialVerwaltung.MaterialTabelle ManagerTable = new de.edlly.gui.materialVerwaltung.MaterialTabelle();
-		ManagerTable.SqlConn = SqlConn;
+		MaterialTabelle ManagerTable = new MaterialTabelle();
+		ManagerTable.SqlConn = sqlConn;
 
-		
-		try{
-		TabMaterialManager.setLayout(null);
-		JScrollPane tableMaterialDBscrollPane = new JScrollPane();
-		// ScrollPane erzeugen für die MaterialTaballe
-		tableMaterialDBscrollPane.setBounds(10, 39, 738, 390);
-		tableMaterialDBscrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
-		tableMaterialDBscrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
-		TabMaterialManager.add(tableMaterialDBscrollPane);
-		
-		// Anzeige Table Hinzufügen
-		tableMaterialDBscrollPane.setViewportView(ManagerTable.AusgabeTable());
-		
-		// TopLabel
-		JLabel lblMaterialDatenbank = new JLabel("Material Datenbank");
-		lblMaterialDatenbank.setFont(new Font("Tahoma", Font.BOLD, 16));
-		lblMaterialDatenbank.setBounds(10, 11, 162, 20);
-		TabMaterialManager.add(lblMaterialDatenbank);
-		
-		// Label über dem Hinzufügen
-		JLabel lblMaterialAnlegen = new JLabel("Material anlegen");
-		lblMaterialAnlegen.setFont(new Font("Tahoma", Font.BOLD, 16));
-		lblMaterialAnlegen.setBounds(10, 440, 138, 20);
-		TabMaterialManager.add(lblMaterialAnlegen);
-		
-		// Material hinzufügen Eingabebereich 
-		JLabel lblGre = new JLabel("Gr\u00F6\u00DFe");
-		lblGre.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblGre.setBounds(10, 468, 34, 14);
-		TabMaterialManager.add(lblGre);
-		
-		txtMatAnlex = new JTextField();
-		txtMatAnlex.setText("50");
-		txtMatAnlex.setBounds(10, 493, 50, 20);
-		TabMaterialManager.add(txtMatAnlex);
-		txtMatAnlex.setColumns(10);
-		
-		JLabel lblX_2 = new JLabel("x");
-		lblX_2.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		lblX_2.setBounds(70, 495, 6, 14);
-		TabMaterialManager.add(lblX_2);
-		
-		txtMatAnlez = new JTextField();
-		txtMatAnlez.setText("10");
-		txtMatAnlez.setBounds(86, 493, 50, 20);
-		TabMaterialManager.add(txtMatAnlez);
-		txtMatAnlez.setColumns(10);
-		
-		// Objekt zum Abfragen der JCombo für die MaterialSorten
-		de.edlly.material.DbAbfrage MaterialSorteList = new de.edlly.material.DbAbfrage();
-		MaterialSorteList.SqlConn = SqlConn;
-		String[] MaterialArtList = MaterialSorteList.SelectTableMaterialSorteString();
-		
-		
-		JComboBox<?> comboMaterialSorte = new JComboBox<Object>(MaterialArtList);
-		comboMaterialSorte.setBounds(156, 493, 96, 20);
-		TabMaterialManager.add(comboMaterialSorte);
-		
-		JLabel lblMaterialSorte = new JLabel("Material Sorte");
-		lblMaterialSorte.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblMaterialSorte.setBounds(156, 468, 81, 14);
-		TabMaterialManager.add(lblMaterialSorte);
-		
-		JLabel lblMaximaleLnge = new JLabel("Maximale L\u00E4nge");
-		lblMaximaleLnge.setFont(new Font("Tahoma", Font.BOLD, 11));
-		lblMaximaleLnge.setBounds(280, 468, 92, 14);
-		TabMaterialManager.add(lblMaximaleLnge);
-		
-		txtMatAnleMax = new JTextField();
-		txtMatAnleMax.setText("4000");
-		txtMatAnleMax.setBounds(280, 493, 86, 20);
-		TabMaterialManager.add(txtMatAnleMax);
-		txtMatAnleMax.setColumns(10);
-		
-		JButton btnMatAnle = new JButton("+");
-		btnMatAnle.setFont(new Font("Tahoma", Font.PLAIN, 11));
-		btnMatAnle.setBounds(392, 492, 43, 23);
-		TabMaterialManager.add(btnMatAnle);
-		
-		JButton btnMarkDelet = new JButton("Makierte Löschen");
-		btnMarkDelet.setBounds(600, 492, 150, 23);
-		TabMaterialManager.add(btnMarkDelet);
-		
-		/**
-		 * Material hinzufügen
-		 */
-		
-		btnMatAnle.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				
-				try{
-					// Daten Hinzufügen
-					SqlConn = de.edlly.db.SQLiteConnect.dbConnection();
-					
-					DbAbfrage MaterialSorteId = new DbAbfrage();
-					MaterialSorteId.SqlConn = SqlConn;
-					int MaterialSorteSelectId = MaterialSorteId.SelectMaterialSorteString((String) comboMaterialSorte.getSelectedItem());
-					
-					DbHinzu MaterialHinzu = new DbHinzu();
-					MaterialHinzu.SqlConn = SqlConn;
-					MaterialHinzu.Add(Integer.parseInt(txtMatAnlex.getText()), Integer.parseInt(txtMatAnlez.getText()), Integer.parseInt(txtMatAnleMax.getText()), MaterialSorteSelectId);
-					
-					// Table erneuern
-					tableMaterialDBscrollPane.setViewportView(ManagerTable.AusgabeTable());
+		try {
+			tabMaterialManager.setLayout(null);
+			JScrollPane tableMaterialDBscrollPane = new JScrollPane();
+			// ScrollPane erzeugen für die MaterialTaballe
+			tableMaterialDBscrollPane.setBounds(10, 39, 738, 390);
+			tableMaterialDBscrollPane.setVerticalScrollBarPolicy(ScrollPaneConstants.VERTICAL_SCROLLBAR_ALWAYS);
+			tableMaterialDBscrollPane.setHorizontalScrollBarPolicy(ScrollPaneConstants.HORIZONTAL_SCROLLBAR_NEVER);
+			tabMaterialManager.add(tableMaterialDBscrollPane);
 
-					
-					
-				}catch(NumberFormatException e){
-					JOptionPane.showMessageDialog(null, "Bitte eine Zahl gültige Zahl eingeben.");	
-					
-				} catch (IllegalArgumentException e ) {
-					JOptionPane.showMessageDialog(null, e.getMessage());
-				
-				}catch (Exception e ) {
-					e.printStackTrace();
-					JOptionPane.showMessageDialog(null, "Exception: "+ e.getClass().getSimpleName() + " " + e.getMessage());
-				}			
-				TabMaterialManager.revalidate();
-				TabMaterialManager.repaint();
-				
-			}
-		});			
-		
-			
-		} catch (Exception e ) {
+			// Anzeige Table Hinzufügen
+			tableMaterialDBscrollPane.setViewportView(ManagerTable.AusgabeTable());
+
+			// TopLabel
+			JLabel lblMaterialDatenbank = new JLabel("Material Datenbank");
+			lblMaterialDatenbank.setFont(new Font("Tahoma", Font.BOLD, 16));
+			lblMaterialDatenbank.setBounds(10, 11, 162, 20);
+			tabMaterialManager.add(lblMaterialDatenbank);
+
+			// Label über dem Hinzufügen
+			JLabel lblMaterialAnlegen = new JLabel("Material anlegen");
+			lblMaterialAnlegen.setFont(new Font("Tahoma", Font.BOLD, 16));
+			lblMaterialAnlegen.setBounds(10, 440, 138, 20);
+			tabMaterialManager.add(lblMaterialAnlegen);
+
+			// Material hinzufügen Eingabebereich
+			JLabel lblGre = new JLabel("Gr\u00F6\u00DFe");
+			lblGre.setFont(new Font("Tahoma", Font.BOLD, 11));
+			lblGre.setBounds(10, 468, 34, 14);
+			tabMaterialManager.add(lblGre);
+
+			txtMatAnlex = new JTextField();
+			txtMatAnlex.setText("50");
+			txtMatAnlex.setBounds(10, 493, 50, 20);
+			tabMaterialManager.add(txtMatAnlex);
+			txtMatAnlex.setColumns(10);
+
+			JLabel lblX_2 = new JLabel("x");
+			lblX_2.setFont(new Font("Tahoma", Font.PLAIN, 11));
+			lblX_2.setBounds(70, 495, 6, 14);
+			tabMaterialManager.add(lblX_2);
+
+			txtMatAnlez = new JTextField();
+			txtMatAnlez.setText("10");
+			txtMatAnlez.setBounds(86, 493, 50, 20);
+			tabMaterialManager.add(txtMatAnlez);
+			txtMatAnlez.setColumns(10);
+
+			// Objekt zum Abfragen der JCombo für die MaterialSorten
+			de.edlly.material.DbAbfrage materialSorteList = new de.edlly.material.DbAbfrage();
+			materialSorteList.SqlConn = sqlConn;
+			String[] MaterialArtList = materialSorteList.SelectTableMaterialSorteString();
+
+			JComboBox<?> comboMaterialSorte = new JComboBox<Object>(MaterialArtList);
+			comboMaterialSorte.setBounds(156, 493, 96, 20);
+			tabMaterialManager.add(comboMaterialSorte);
+
+			JLabel lblMaterialSorte = new JLabel("Material Sorte");
+			lblMaterialSorte.setFont(new Font("Tahoma", Font.BOLD, 11));
+			lblMaterialSorte.setBounds(156, 468, 81, 14);
+			tabMaterialManager.add(lblMaterialSorte);
+
+			JLabel lblMaximaleLnge = new JLabel("Maximale L\u00E4nge");
+			lblMaximaleLnge.setFont(new Font("Tahoma", Font.BOLD, 11));
+			lblMaximaleLnge.setBounds(280, 468, 92, 14);
+			tabMaterialManager.add(lblMaximaleLnge);
+
+			txtMatAnleMax = new JTextField();
+			txtMatAnleMax.setText("4000");
+			txtMatAnleMax.setBounds(280, 493, 86, 20);
+			tabMaterialManager.add(txtMatAnleMax);
+			txtMatAnleMax.setColumns(10);
+
+			JButton btnMatAnle = new JButton("+");
+			btnMatAnle.setFont(new Font("Tahoma", Font.PLAIN, 11));
+			btnMatAnle.setBounds(392, 492, 43, 23);
+			tabMaterialManager.add(btnMatAnle);
+
+			JButton btnMarkDelet = new JButton("Makierte Löschen");
+			btnMarkDelet.setBounds(600, 492, 150, 23);
+			tabMaterialManager.add(btnMarkDelet);
+
+			/**
+			 * Material hinzufügen
+			 */
+
+			btnMatAnle.addActionListener(new ActionListener() {
+				public void actionPerformed(ActionEvent arg0) {
+
+					try {
+						// Daten Hinzufügen
+						sqlConn = de.edlly.db.SQLiteConnect.dbConnection();
+
+						DbAbfrage materialSorteId = new DbAbfrage();
+						materialSorteId.SqlConn = sqlConn;
+						int MaterialSorteSelectId = materialSorteId
+								.SelectMaterialSorteString((String) comboMaterialSorte.getSelectedItem());
+
+						DbHinzu materialHinzu = new DbHinzu();
+						materialHinzu.SqlConn = sqlConn;
+						materialHinzu.Add(Integer.parseInt(txtMatAnlex.getText()),
+								Integer.parseInt(txtMatAnlez.getText()), Integer.parseInt(txtMatAnleMax.getText()),
+								MaterialSorteSelectId);
+
+						// Table erneuern
+						tableMaterialDBscrollPane.setViewportView(ManagerTable.AusgabeTable());
+
+					} catch (NumberFormatException e) {
+						JOptionPane.showMessageDialog(null, "Bitte eine Zahl gültige Zahl eingeben.");
+
+					} catch (IllegalArgumentException e) {
+						JOptionPane.showMessageDialog(null, e.getMessage());
+
+					} catch (Exception e) {
+						e.printStackTrace();
+						JOptionPane.showMessageDialog(null,
+								"Exception: " + e.getClass().getSimpleName() + " " + e.getMessage());
+					}
+					tabMaterialManager.revalidate();
+					tabMaterialManager.repaint();
+
+				}
+			});
+
+		} catch (Exception e) {
 			e.printStackTrace();
-			JOptionPane.showMessageDialog(null, "Exception: "+ e.getClass().getSimpleName() + " " + e.getMessage());
-		
+			JOptionPane.showMessageDialog(null, "Exception: " + e.getClass().getSimpleName() + " " + e.getMessage());
+
 		}
-	
-		
-		return TabMaterialManager;
+
+		return tabMaterialManager;
 	}
-	
-	
 
 }
