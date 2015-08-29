84f9ed5baf3e56ac6ecaa7c1f3dd9624803e9701
Edll <edlly@edlly.de>
2015-08-29 10:36:25 +0200

    Formatierung
    Formatierung des Codes Überarbeitet

--- src/de/edlly/material/NeuerMaterialDatensatz.java
+++ src/de/edlly/material/NeuerMaterialDatensatz.java
@@ -4,132 +4,126 @@ import java.sql.*;
 import de.edlly.material.MaterialKonstanten;
 
 /**
- * Legt einen neuen Material Datensatz aus den objektVariabeln an.
- * 
- * TODO updateVisibly in eine geeignete Klasse verschieben.
+ * Legt einen neuen Material Datensatz aus den objektVariabeln an die im Set
+ * geprüft werden
  * 
  * @author Edlly java@edlly.de
  *
  */
 public class NeuerMaterialDatensatz {
 
-	private int koordinateX;
-	private int koordinateZ;
-	private int koordinateyMax;
-	private int materialSorteId;
-	private Connection sqlConnection;
+    private int koordinateX;
+    private int koordinateZ;
+    private int koordinateyMax;
+    private int materialSorteId;
 
-	public NeuerMaterialDatensatz(Connection SqlConnection) {
-		this.sqlConnection = SqlConnection;
+    private Connection sqlConnection;
 
+    public NeuerMaterialDatensatz(Connection SqlConnection) {
+	this.sqlConnection = SqlConnection;
+    }
+
+    public void setMaterialDaten(int koordinateX, int koordinateZ, int koordinateyMax, int materialSorteId) {
+
+	if (koordinateXIstImDefinitionsbereich(koordinateX)) {
+	    this.koordinateX = koordinateX;
 	}
 
-	public void setMaterialDaten(int koordinateX, int koordinateZ, int koordinateyMax, int materialSorteId) {
-
-		if (koordinateXIstImDefinitionsbereich(koordinateX)) {
-			this.koordinateX = koordinateX;
-		}
-
-		if (koordinateZIstImDefinitionsbereich(koordinateZ)) {
-			this.koordinateZ = koordinateZ;
-		}
-
-		if (koordinateyMaxIstImDefinitionsbereich(koordinateyMax)) {
-			this.koordinateyMax = koordinateyMax;
-		}
-
-		if (materialSorteIdIstVorhanden(materialSorteId)) {
-			this.materialSorteId = materialSorteId;
-		}
-
+	if (koordinateZIstImDefinitionsbereich(koordinateZ)) {
+	    this.koordinateZ = koordinateZ;
 	}
 
-	public Boolean datensatzAusObjektWertenAnlegen() throws SQLException {
-
-		PreparedStatement sqlPreparedStatment = null;
-
-		try {
-			String query = "INSERT INTO Material (\"MaterialSorteId\",\"x\",\"z\",\"yMax\",\"visibly\") VALUES (?1,?2,?3,?4,?5)";
-
-			sqlPreparedStatment = sqlConnection.prepareStatement(query);
-
-			sqlPreparedStatment.setInt(1, this.materialSorteId);
-			sqlPreparedStatment.setInt(2, this.koordinateX);
-			sqlPreparedStatment.setInt(3, this.koordinateZ);
-			sqlPreparedStatment.setInt(4, this.koordinateyMax);
-			sqlPreparedStatment.setBoolean(5, true);
-			sqlPreparedStatment.executeUpdate();
-
-		} catch (SQLException e) {
-			
-			e.printStackTrace();
-			throw new SQLException(e);
-		} finally {
-			try {
-				sqlPreparedStatment.close();
-			} catch (Exception e) {
-				
-				e.printStackTrace();
-			}
-		}
-		return true;
+	if (koordinateyMaxIstImDefinitionsbereich(koordinateyMax)) {
+	    this.koordinateyMax = koordinateyMax;
 	}
 
-	public Boolean koordinateXIstImDefinitionsbereich(int koordinateX) {
+	if (materialSorteIdIstVorhanden(materialSorteId)) {
+	    this.materialSorteId = materialSorteId;
+	}
+    }
 
-		if (koordinateX <= 0) {
-			throw new IllegalArgumentException("Die Materialbreite darf nicht Negativ oder 0 sein.");
-		}
+    public Boolean datensatzAusObjektWertenAnlegen() throws SQLException {
 
-		if (koordinateX > MaterialKonstanten.MAXIMALER_X_WERT) {
-			throw new IllegalArgumentException(
-					"Die maximal Material Breite ist: " + MaterialKonstanten.MAXIMALER_X_WERT);
-		}
+	PreparedStatement sqlPreparedStatment = null;
+	try {
+	    String query = "INSERT INTO Material (\"MaterialSorteId\",\"x\",\"z\",\"yMax\",\"visibly\") VALUES (?1,?2,?3,?4,?5)";
 
-		return true;
+	    sqlPreparedStatment = sqlConnection.prepareStatement(query);
+	    sqlPreparedStatment.setInt(1, this.materialSorteId);
+	    sqlPreparedStatment.setInt(2, this.koordinateX);
+	    sqlPreparedStatment.setInt(3, this.koordinateZ);
+	    sqlPreparedStatment.setInt(4, this.koordinateyMax);
+	    sqlPreparedStatment.setBoolean(5, true);
+
+	    sqlPreparedStatment.executeUpdate();
+
+	} catch (SQLException e) {
+	    e.printStackTrace();
+	    throw new SQLException(e);
+
+	} finally {
+	    try {
+		sqlPreparedStatment.close();
+
+	    } catch (Exception e) {
+		e.printStackTrace();
+
+	    }
+	}
+	return true;
+    }
+
+    public Boolean koordinateXIstImDefinitionsbereich(int koordinateX) {
+
+	if (koordinateX <= 0) {
+	    throw new IllegalArgumentException("Die Materialbreite darf nicht Negativ oder 0 sein.");
 	}
 
-	public Boolean koordinateZIstImDefinitionsbereich(int koordinateZ) {
+	if (koordinateX > MaterialKonstanten.MAXIMALER_X_WERT) {
+	    throw new IllegalArgumentException(
+		    "Die maximal Material Breite ist: " + MaterialKonstanten.MAXIMALER_X_WERT);
+	}
+	return true;
+    }
 
-		if (koordinateZ <= 0) {
-			throw new IllegalArgumentException("Die Materialbreite darf nicht Negativ oder 0 sein.");
-		}
+    public Boolean koordinateZIstImDefinitionsbereich(int koordinateZ) {
 
-		if (koordinateZ > MaterialKonstanten.MAXIMALER_Z_WERT) {
-			throw new IllegalArgumentException(
-					"Die maximal Material Dicke ist: " + MaterialKonstanten.MAXIMALER_Z_WERT);
-		}
-
-		return true;
+	if (koordinateZ <= 0) {
+	    throw new IllegalArgumentException("Die Materialbreite darf nicht Negativ oder 0 sein.");
 	}
 
-	public Boolean koordinateyMaxIstImDefinitionsbereich(int koordinatey) {
+	if (koordinateZ > MaterialKonstanten.MAXIMALER_Z_WERT) {
+	    throw new IllegalArgumentException(
+		    "Die maximal Material Dicke ist: " + MaterialKonstanten.MAXIMALER_Z_WERT);
+	}
+	return true;
+    }
 
-		if (koordinatey <= 0) {
-			throw new IllegalArgumentException("Die Materialbreite darf nicht Negativ sein.");
-		}
+    public Boolean koordinateyMaxIstImDefinitionsbereich(int koordinatey) {
 
-		if (koordinatey > MaterialKonstanten.MAXIMALER_Y_WERT) {
-			throw new IllegalArgumentException(
-					"Die maximal Material Länge ist: " + MaterialKonstanten.MAXIMALER_Y_WERT);
-		}
-
-		return true;
+	if (koordinatey <= 0) {
+	    throw new IllegalArgumentException("Die Materialbreite darf nicht Negativ sein.");
 	}
 
-	public Boolean materialSorteIdIstVorhanden(int materialSorteId) {
-
-		if (materialSorteId < 0) {
-			throw new IllegalArgumentException("Die materialSorteId darf nicht Negativ sein.");
-		}
-		/**
-		 * @TODO Abfrage einbauen die abfragt ob die Sorte in der Datenbank
-		 *       vorhanden ist.
-		 */
-
-		return true;
+	if (koordinatey > MaterialKonstanten.MAXIMALER_Y_WERT) {
+	    throw new IllegalArgumentException(
+		    "Die maximal Material Länge ist: " + MaterialKonstanten.MAXIMALER_Y_WERT);
 	}
+	return true;
+    }
 
-	
+    
+    public Boolean materialSorteIdIstVorhanden(int materialSorteId) {
+
+	if (materialSorteId < 0) {
+	    throw new IllegalArgumentException("Die materialSorteId darf nicht Negativ sein.");
+	}
+	/**
+	 * @TODO Abfrage einbauen die abfragt ob die Sorte in der Datenbank
+	 *       vorhanden ist.
+	 */
+
+	return true;
+    }
 
 }
