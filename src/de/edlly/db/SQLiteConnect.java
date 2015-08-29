87c0969d733e9ce5878ee41f47b973d2a4a604b5
Edll <edlly@edlly.de>
2015-08-29 10:49:32 +0200

    Close sqlConnection eingefügt
    neue Methode sqlConnection eingefügt.

--- src/de/edlly/db/SQLiteConnect.java
+++ src/de/edlly/db/SQLiteConnect.java
@@ -1,32 +1,37 @@
 package de.edlly.db;
 
 import java.sql.*;
+
 /**
- * Attribute:
- * Connection conn enthält die Aktuelle Connection für die SQL
+ * Attribute: Connection conn enthält die Aktuelle Connection für die SQL
  * 
- * Methoden:
- * dbConnection() Stellt die Verbindung zu Datenbank her.
+ * Methoden: dbConnection() Stellt die Verbindung zu Datenbank her.
  * 
  * @author Edlly
  *
  */
 
-public class SQLiteConnect{
-	Connection conn=null;
-	
-	public static Connection dbConnection ()
-	{
-		try{
-			Class.forName("org.sqlite.JDBC");
-			Connection conn=DriverManager.getConnection("jdbc:sqlite:kupfer.sqlite");
-			return conn;
-		}catch(Exception e){
-			throw new IllegalArgumentException("Verbindung zur SQLite DB nicht möglich");	
-		}
-		
+public class SQLiteConnect {
+    Connection conn = null;
+
+    public static Connection dbConnection() {
+	try {
+	    Class.forName("org.sqlite.JDBC");
+	    Connection conn = DriverManager.getConnection("jdbc:sqlite:kupfer.sqlite");
+	    return conn;
+	} catch (Exception e) {
+	    throw new IllegalArgumentException("Verbindung zur SQLite DB nicht möglich");
 	}
-	
-	
+
+    }
+
+    public void closeSqlConnection(Connection sqlConnection) {
+
+	try {
+	    sqlConnection.close();
+	} catch (SQLException e) {
+	    e.printStackTrace();
+	}
+    }
 
 }
