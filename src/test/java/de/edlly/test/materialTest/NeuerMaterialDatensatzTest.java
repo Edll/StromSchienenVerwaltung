4967f0b04069788c3b4123b0a1283b4d6f530e78
Edll <java@edlly.de>
2015-09-02 18:48:00 +0200

    Auf einen JUnit test umgearbeitet.
    Von den eigenen Test auf einen JUnit test geändert.

--- src/test/java/de/edlly/test/materialTest/NeuerMaterialDatensatzTest.java
+++ src/test/java/de/edlly/test/materialTest/NeuerMaterialDatensatzTest.java
@@ -1,6 +1,10 @@
 package de.edlly.test.materialTest;
 
 import java.sql.Connection;
+import java.sql.SQLException;
+
+import org.junit.*;
+import org.junit.rules.ExpectedException;
 import de.edlly.db.SQLiteConnect;
 import de.edlly.material.MaterialKonstanten;
 import de.edlly.material.NeuerMaterialDatensatz;
@@ -15,88 +19,55 @@ import junit.framework.TestCase;
 
 public class NeuerMaterialDatensatzTest extends TestCase {
 
-    NeuerMaterialDatensatz testNeuerMaterialDatensatz;
+    private NeuerMaterialDatensatz materialDatensatzTest;
     private Connection sqlConnection;
+
+    private int koordinatenMaxX = MaterialKonstanten.MAXIMALER_X_WERT;
+    private int koordiantenMaxZ = MaterialKonstanten.MAXIMALER_Z_WERT;
+    private int koordiantenMaxY = MaterialKonstanten.MAXIMALER_Y_WERT;
+
+    private int koordinatenMinX = 1;
+    private int koordinatenMinY = 1;
+    private int koordiantenMinZ = 1;
+
+    private int sortenId = 1;
 
     @Override
     public void setUp() {
 	sqlConnection = SQLiteConnect.dbConnection();
-
+	materialDatensatzTest = new NeuerMaterialDatensatz(sqlConnection);
     }
 
-    public void testMaximaleObjektDatensetzen() {
-	assertTrue("Maximale werte in Objekt setzen", maximaleObjektDatensetzen());
+    @Rule
+    public ExpectedException thrown = ExpectedException.none();
+
+    @Test
+    public void testSetMaximaleMaterialDaten() throws IllegalArgumentException {
+
+	materialDatensatzTest.setMaterialDaten(koordinatenMaxX, koordiantenMaxZ, koordiantenMaxY, sortenId);
+	thrown.expect(IllegalArgumentException.class);
     }
 
-    public void testMinimaleObjektDatensetzen() {
-	assertTrue("Minimale werte in Objekt setzen", minimaleObjektDatensetzen());
+    @Test
+    public void testMinimaleObjektDatensetzen() throws IllegalArgumentException {
+
+	materialDatensatzTest.setMaterialDaten(koordinatenMinX, koordiantenMinZ, koordinatenMinY, sortenId);
+	thrown.expect(IllegalArgumentException.class);
     }
 
-    public void testNeuerDatensatztausObjektDaten() {
-	assertTrue("Neuer Datensatz aus Objektdaten", neuerDatensatztAusObjektDatenErzeugen());
+    @Test
+    public void testNeuerDatensatztausObjektDaten() throws IllegalArgumentException, SQLException {
+
+	materialDatensatzTest.setMaterialDaten(koordinatenMinX, koordiantenMinZ, koordinatenMinY, sortenId);
+
+	boolean materialDatenEingetragen = materialDatensatzTest.datensatzAusObjektWertenAnlegen();
+
+	assertTrue("Neuer Datensatz aus Objektdaten", materialDatenEingetragen);
     }
 
     @Override
     public void tearDown() {
 	SQLiteConnect.closeSqlConnection(sqlConnection);
-
-    }
-
-    private Boolean maximaleObjektDatensetzen() {
-	int koordinatenMaxX = MaterialKonstanten.MAXIMALER_X_WERT;
-	int koordiantenMaxZ = MaterialKonstanten.MAXIMALER_Z_WERT;
-	int koordiantenMaxY = MaterialKonstanten.MAXIMALER_Y_WERT;
-	int sortenId = 1;
-
-	try {
-	    NeuerMaterialDatensatz objektfuerDatensatz = new NeuerMaterialDatensatz(sqlConnection);
-	    objektfuerDatensatz.setMaterialDaten(koordinatenMaxX, koordiantenMaxZ, koordiantenMaxY, sortenId);
-
-	} catch (Exception e) {
-
-	    e.printStackTrace();
-	    return false;
-	}
-	return true;
-    }
-
-    private Boolean minimaleObjektDatensetzen() {
-	int koordinatenMinX = 1;
-	int koordinatenMinY = 1;
-	int koordiantenMinZ = 1;
-	int sortenId = 1;
-
-	try {
-	    NeuerMaterialDatensatz objektfuerDatensatz = new NeuerMaterialDatensatz(sqlConnection);
-	    objektfuerDatensatz.setMaterialDaten(koordinatenMinX, koordiantenMinZ, koordinatenMinY, sortenId);
-
-	} catch (Exception e) {
-	    e.printStackTrace();
-	    return false;
-
-	}
-	return true;
-    }
-
-    private Boolean neuerDatensatztAusObjektDatenErzeugen() {
-	int koordinatenMaxX = MaterialKonstanten.MAXIMALER_X_WERT;
-	int koordiantenMaxZ = MaterialKonstanten.MAXIMALER_Z_WERT;
-	int koordiantenMaxY = MaterialKonstanten.MAXIMALER_Y_WERT;
-	int sortenId = 1;
-
-	try {
-	    NeuerMaterialDatensatz objektfuerDatensatz = new NeuerMaterialDatensatz(sqlConnection);
-	    objektfuerDatensatz.setMaterialDaten(koordinatenMaxX, koordiantenMaxZ, koordiantenMaxY, sortenId);
-	    if (objektfuerDatensatz.datensatzAusObjektWertenAnlegen()) {
-		return true;
-	    } else {
-		return false;
-	    }
-
-	} catch (Exception e) {
-	    e.printStackTrace();
-	    return false;
-	}
 
     }
 
