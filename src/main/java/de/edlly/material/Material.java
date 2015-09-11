5b82ff3a2dd0690e77189251839b388c5d6d08f3
Edll <java@edlly.de>
2015-09-11 19:34:40 +0200

    Update - abstract class
    * Klasse in abstract class umgeändert.

--- src/main/java/de/edlly/material/Material.java
+++ src/main/java/de/edlly/material/Material.java
@@ -4,8 +4,14 @@ import java.sql.Connection;
 
 import de.edlly.db.SQLiteAbfrage;
 import de.edlly.db.SQLiteConnect;
-
-public class Material extends SQLiteAbfrage {
+/**
+ * 
+ * Oberklasse für alle Objekte vom Typ Material
+ * 
+ * @author Edlly java@edlly.de
+ *
+ */
+public abstract class Material extends SQLiteAbfrage {
 
     public Material(Connection sqlConnection) {
 	SQLiteConnect.sqlConnectionCloseorNull(sqlConnection);
