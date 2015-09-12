51fe1d2cf212b99e7a1d440599a7c402a696a91b
Edlly java@edlly.de <Edlly java@edlly.de@AUSTIN_MOBILE>
2015-09-12 10:18:44 +0200

    Update - Klassenname
    * in AllTests umbenannt damit maven die test nicht doppelt ausführt

---
+++ src/test/java/de/edlly/test/AllTests.java
@@ -0,0 +1,41 @@
+package de.edlly.test;
+
+import de.edlly.test.materialTest.NeuerMaterialDatensatzTest;
+import de.edlly.test.materialTest.MaterialListeTest;
+import de.edlly.test.materialTest.MaterialSorteTest;
+import de.edlly.test.materialTest.MaterialDatensatzTest;
+import de.edlly.test.materialTest.MaterialIdsTest;
+import de.edlly.test.materialTest.UpdateMaterialDatensatzTest;
+import de.edlly.test.db.SQLiteAbfrageTest;
+import de.edlly.test.db.SQLiteBooleanTest;
+import de.edlly.test.db.SQLiteConnectTest;
+import junit.framework.Test;
+import junit.framework.TestSuite;
+
+/**
+ * Lädt die einzelnen tests.
+ * 
+ * @author Edlly java@edlly.de
+ *
+ */
+
+public class AllTests extends TestSuite {
+
+    public static Test suite() {
+	TestSuite materialDatensatz = new TestSuite("Material Datenbank Abfragen");
+	materialDatensatz.addTestSuite(NeuerMaterialDatensatzTest.class);
+	materialDatensatz.addTestSuite(MaterialDatensatzTest.class);
+	materialDatensatz.addTestSuite(UpdateMaterialDatensatzTest.class);
+	materialDatensatz.addTestSuite(MaterialListeTest.class);
+	materialDatensatz.addTestSuite(MaterialIdsTest.class);
+	materialDatensatz.addTestSuite(MaterialSorteTest.class);
+	
+
+	materialDatensatz.addTestSuite(SQLiteBooleanTest.class);
+	materialDatensatz.addTestSuite(SQLiteConnectTest.class);
+	materialDatensatz.addTestSuite(SQLiteAbfrageTest.class);
+	
+	
+	return materialDatensatz;
+    }
+}
