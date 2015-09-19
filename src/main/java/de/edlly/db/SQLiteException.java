07ed058f5cedf7ae8b38b37e87e12692460f33f6
Edlly java@edlly.de <Edlly java@edlly.de@AUSTIN_MOBILE>
2015-09-19 10:14:56 +0200

    New - SQLiteException Klasse
    + Für eigene SQLiteException behandlung, eine extra ExceptionKlasse
    eingefügt.

---
+++ src/main/java/de/edlly/db/SQLiteException.java
@@ -0,0 +1,17 @@
+package de.edlly.db;
+
+public class SQLiteException extends RuntimeException {
+    private static final long serialVersionUID = 1486454907602162417L;
+    
+    public SQLiteException(){
+	
+    }
+    public SQLiteException(String s){
+	super(s);
+    }
+    
+
+    
+    
+    
+}
