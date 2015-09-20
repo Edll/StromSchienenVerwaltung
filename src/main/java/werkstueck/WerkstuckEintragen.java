c5d138bdfe49f3cbee7e4b5a61b1a4da002126a9
Edll <java@edlly.de>
2015-09-20 18:19:14 +0200

    Add - Erste version des Hinzufügens von neuen Werkstücken
    + Unterklasse WerkstueckEintragen

---
+++ src/main/java/werkstueck/WerkstuckEintragen.java
@@ -0,0 +1,48 @@
+package werkstueck;
+
+import java.sql.SQLException;
+
+import de.edlly.db.SQLiteConnect;
+import de.edlly.db.SQLitePreparedStatement;
+
+public class WerkstuckEintragen<T> extends WerkstueckDatensatz<T> {
+    SQLitePreparedStatement sql;
+
+    public WerkstuckEintragen(SQLiteConnect sqlConnection) throws WerkstueckException {
+	super(sqlConnection);
+	try {
+	sql = new SQLitePreparedStatement(sqlConnection);
+	} catch (IllegalArgumentException e) {
+
+	    throw new WerkstueckException("Format Fehler:" + e.getLocalizedMessage() + " in " + e.getClass());
+	} catch (SQLException e) {
+
+	    throw new WerkstueckException("SQL Fehler:" + e.getLocalizedMessage() + " in " + e.getClass());
+	}
+    }
+    
+    public boolean dbAdd() throws WerkstueckException{
+	try {
+	    sql.setQuery("INSERT INTO Werkstueck (\"materialId\",\"name\",\"projektNr\",\"erstellDatum\") VALUES (?1,?2,?3,?4)");
+	    sql.preparedStatmentVorbereiten(sql.getQuery());
+	    sql.getPreparedStatment().setInt(1, getMaterialId());
+	    sql.getPreparedStatment().setString(2, getName());
+	    sql.getPreparedStatment().setInt(1, getProjektNr());
+	    sql.getPreparedStatment().setLong(1, getErstellDatum());
+	    
+	    sql.preparedStatmentAusfuehren();
+	    sql.closePrepareStatment();
+	    return true;
+	} catch (SQLException e) {
+	    
+	    throw new WerkstueckException("SQL Fehler:" + e.getLocalizedMessage() + " in " + e.getClass());
+	}finally{
+	    try {
+		sql.closePrepareStatment(); 
+	    } catch (SQLException e) {
+		  throw new WerkstueckException("SQL Fehler:" + e.getLocalizedMessage() + " in " + e.getClass());
+	    }
+	}
+    }
+
+}
---
+++ src/test/java/werkstueck/WerkstuckEintragenTest.java
@@ -0,0 +1,37 @@
+package werkstueck;
+
+import java.sql.SQLException;
+
+import org.junit.Test;
+
+import de.edlly.db.SQLiteConnect;
+import junit.framework.TestCase;
+
+public class WerkstuckEintragenTest extends TestCase {
+    SQLiteConnect sqlConnection;
+    WerkstuckEintragen<?> datensatz;
+    
+    @Override
+    public void setUp() throws WerkstueckException {
+	sqlConnection = new SQLiteConnect();
+	sqlConnection.dbConnect();
+	datensatz = new  WerkstuckEintragen<IWerkstueckDatensatz<?>>(sqlConnection);
+    }
+
+    @Test
+    public void testDBAdd() throws IllegalArgumentException, WerkstueckException {
+	 java.util.Date date = new java.util.Date();
+	datensatz.setDatensatz("TestDaten", 1, date.getTime(), 666);
+	
+	boolean condition = datensatz.dbAdd();
+	
+	assertTrue("Das Eintragen ist fehlgeschlagen", condition);
+    }
+
+    @Override
+    public void tearDown() throws SQLException {
+	sqlConnection.close();
+    }
+    
+
+}
