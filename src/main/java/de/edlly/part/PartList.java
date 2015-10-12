885e76c880c61c479da2b1834b7b204988692370
Edll <edlly@ubuntu>
2015-10-12 17:37:31 +0200

    Change - Einrückungen
    * Einrückung auf Ubuntu System verändert

--- src/main/java/de/edlly/part/PartList.java
+++ src/main/java/de/edlly/part/PartList.java
@@ -8,115 +8,115 @@ import java.util.Collections;
 import de.edlly.db.*;
 
 public class PartList implements IPartList {
-    private SQLiteStatement sqlStatment;
+	private SQLiteStatement sqlStatment;
 
-    private IPart part = new Part();
-    private List<IPart> list = new ArrayList<IPart>();
-    private List<Integer> idList;
+	private IPart part = new Part();
+	private List<IPart> list = new ArrayList<IPart>();
+	private List<Integer> idList;
 
-    public PartList(SQLiteConnect sqlConnection) throws PartException, SQLiteException {
-	SQLiteConnect.isClosedOrNull(sqlConnection);
-	sqlStatment = new SQLiteStatement(sqlConnection);
+	public PartList(SQLiteConnect sqlConnection) throws PartException, SQLiteException {
+		SQLiteConnect.isClosedOrNull(sqlConnection);
+		sqlStatment = new SQLiteStatement(sqlConnection);
 
-    }
-
-    @Override
-    public IPart getPart() {
-	return part;
-    }
-
-    @Override
-    public void setPart(IPart part) {
-	this.part = part;
-    }
-
-    @Override
-    public List<Integer> getIdList() throws SQLiteException {
-	idListeErstellen();
-	return idList;
-    }
-
-    @Override
-    public void sortList() {
-	Collections.sort(list);
-    }
-
-    @Override
-    public List<IPart> getDataList() throws PartException, SQLiteException {
-	makeCompleteDataList();
-	return list;
-    }
-
-    @Override
-    public List<IPart> getDataListSort() throws PartException, SQLiteException {
-	makeCompleteDataList();
-	sortList();
-	return list;
-    }
-
-    private void makeCompleteDataList() throws SQLiteException, PartException {
-	for (int i = 0; i < getIdList().size(); i++) {
-	    part = new Part();
-	    part.getDB(getIdList().get(i), sqlStatment);
-
-	    list.add(part);
 	}
-    }
 
-    @Override
-    public List<IPart> getDataList(int... id) throws PartException, SQLiteException {
-	for (int i = 0; i < id.length; i++) {
-
-	    part = new Part();
-	    part.getDB(id[i], sqlStatment);
-
-	    list.add(part);
+	@Override
+	public IPart getPart() {
+		return part;
 	}
-	return list;
 
-    }
+	@Override
+	public void setPart(IPart part) {
+		this.part = part;
+	}
 
-    @Override
-    public void idListeErstellen() throws SQLiteException {
-	try {
-	    idList = new ArrayList<Integer>();
-	    sqlStatment.setQuery("Select id From Werkstueck");
-	    sqlStatment.statmentVorbereitenUndStarten(sqlStatment.getQuery());
-	    int anzahlId = sqlStatment.anzahlDatenseatze();
+	@Override
+	public List<Integer> getIdList() throws SQLiteException {
+		idListeErstellen();
+		return idList;
+	}
 
-	    if (anzahlId == 0) {
-		idList.add(new Integer(0));
-	    } else {
+	@Override
+	public void sortList() {
+		Collections.sort(list);
+	}
 
-		sqlStatment.statmentVorbereitenUndStarten(sqlStatment.getQuery());
-		while (sqlStatment.getResult().next()) {
-		    idList.add(sqlStatment.getResult().getInt(1));
+	@Override
+	public List<IPart> getDataList() throws PartException, SQLiteException {
+		makeCompleteDataList();
+		return list;
+	}
+
+	@Override
+	public List<IPart> getDataListSort() throws PartException, SQLiteException {
+		makeCompleteDataList();
+		sortList();
+		return list;
+	}
+
+	private void makeCompleteDataList() throws SQLiteException, PartException {
+		for (int i = 0; i < getIdList().size(); i++) {
+			part = new Part();
+			part.getDB(getIdList().get(i), sqlStatment);
+
+			list.add(part);
 		}
-	    }
-	    sqlStatment.closeStatmentAndResult();
-
-	} catch (SQLException sqlException) {
-	    throw new SQLiteException(sqlException.getLocalizedMessage());
-
-	} finally {
-	    sqlStatment.closeStatmentAndResult();
 	}
-    }
 
-    @Override
-    public boolean idVorhanden(int id) throws SQLiteException {
+	@Override
+	public List<IPart> getDataList(int... id) throws PartException, SQLiteException {
+		for (int i = 0; i < id.length; i++) {
 
-	if (getIdList().get(0) == 0) {
+			part = new Part();
+			part.getDB(id[i], sqlStatment);
 
-	    return false;
-	} else {
-	    for (int i = 0; i < getIdList().size(); i++) {
-
-		if (getIdList().get(i) == id) {
-		    return true;
+			list.add(part);
 		}
-	    }
-	    return false;
+		return list;
+
 	}
-    }
+
+	@Override
+	public void idListeErstellen() throws SQLiteException {
+		try {
+			idList = new ArrayList<Integer>();
+			sqlStatment.setQuery("Select id From Werkstueck");
+			sqlStatment.statmentVorbereitenUndStarten(sqlStatment.getQuery());
+			int anzahlId = sqlStatment.anzahlDatenseatze();
+
+			if (anzahlId == 0) {
+				idList.add(new Integer(0));
+			} else {
+
+				sqlStatment.statmentVorbereitenUndStarten(sqlStatment.getQuery());
+				while (sqlStatment.getResult().next()) {
+					idList.add(sqlStatment.getResult().getInt(1));
+				}
+			}
+			sqlStatment.closeStatmentAndResult();
+
+		} catch (SQLException sqlException) {
+			throw new SQLiteException(sqlException.getLocalizedMessage());
+
+		} finally {
+			sqlStatment.closeStatmentAndResult();
+		}
+	}
+
+	@Override
+	public boolean idVorhanden(int id) throws SQLiteException {
+
+		if (getIdList().get(0) == 0) {
+
+			return false;
+		} else {
+			for (int i = 0; i < getIdList().size(); i++) {
+
+				if (getIdList().get(i) == id) {
+					return true;
+				}
+			}
+			return false;
+		}
+	}
 }
